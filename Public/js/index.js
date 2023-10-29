cc5adc090ed23e3f7a8cc8f93aacda06e422d4ba
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-10-29 19:58:55 +0800

    add login,update index

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,1 +1,9 @@
-const carousel = new bootstrap.Carousel('#myCarousel')
+document.getElementById("loginButton").addEventListener("click", function() {
+    // Mengarahkan pengguna ke halaman lain, ganti URL "halaman_tujuan.html" dengan URL yang sesuai
+    window.location.href = "login.html";
+});
+
+document.getElementById("cariButton").addEventListener("click", function() {
+    // Mengarahkan pengguna ke halaman lain, ganti URL "halaman_tujuan.html" dengan URL yang sesuai
+    window.location.href = "carikost.html";
+});
